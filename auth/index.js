327e967f00518d6cde8e61ea580c9682a4125096
Mario Castro <mariocastro.pva@gmail.com>
2022-11-07 16:34:08 -0400

    Misc

--- auth/index.js
+++ auth/index.js
@@ -1,9 +1,44 @@
+const config = require('../config');
 const jwt = require('jsonwebtoken');
 
 function sign(data) {
-    return jwt.sign(data, 'superSecret');
+    return jwt.sign(data, config.jwt.secret);
+}
+
+function verify(token) {
+    return jwt.verify(token, config.jwt.secret)
+}
+
+const check = {
+    own: function(req, owner) {
+        const decoded = decodeHeader(req);
+        console.log(decoded);
+    }
+}
+
+function getToken(auth) {
+    if (!auth) {
+        throw new Error('no  viene token');
+    }
+
+    if (auth.indexOf('Bearer ') === -1) {
+        throw new Error('Formato invalido');
+    }
+
+    let token = auth.replace('Bearer ', '');
+    return token;
+}
+
+function decodeHeader(req) {
+    const authorization = req.headers.authorization || '';
+    const token = getToken(authorization);
+    const decoded = verify(token);
+
+    req.user = decoded;
 }
 
 module.exports = {
-    sign
+    sign,
+    decodeHeader,
+    getToken
 };
--- config.js
+++ config.js
@@ -2,5 +2,8 @@ module.exports = {
     api: {
         host: process.env.HOST || 'localhost',
         port: process.env.API_PORT || 3000,
+    },
+    jwt: {
+        secret: process.env.JWT_SECRET || 'notASecret',
     }
 }
